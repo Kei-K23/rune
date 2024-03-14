a0175ead7e4e25c90787573669331e517b686120
Kei-K23 <keiksl2301@gmail.com>
2024-03-14 18:36:58 +0630

    add rune core main function rune

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+
+interface IRune<T> {
+  get: () => T
+  set: (value: T) => void
+  subscribe: (callback: (value: T) => void) => void
+}
+
+type Getter<T> = (get: <K>(r: IRune<K>) => K) => T
+
+/**
+ * Creates a new rune that wraps the given value.
+ * @param value - the initial value of the rune
+ * @returns a new rune that wraps the given value
+ */
+export function rune<T>(value: T | Getter<T> | null): IRune<T> {
+  function get<K>(rune: IRune<K>): K {
+    return rune.get();
+  }
+
+  // initial value
+  let v: T;
+
+  if (typeof value === "function") {
+    // If `value` is an action function, execute it with a `get` function
+    v = (value as Getter<T>)(get);
+  } else {
+    // If `value` is not a function, directly assign it to `v`
+    v = value as T;
+  }
+
+  // create a new subscriber that store function
+  const subscribers = new Set<(newValue: T) => void>()
+
+  return {
+    get: () => v,
+    // update the value with the new value and also update the subscribers
+    set: (value: T) => {
+      v = value
+      subscribers.forEach(callback => callback(value))
+    },
+    // subscribe the callback function
+    subscribe: (callback: (value: T) => void) => {
+      subscribers.add(callback)
+    }
+  }
+}
